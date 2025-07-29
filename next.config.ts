2aec0db9a7735032573895d681261c9f856da215
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-07-29 21:58:43 +0300

    Trying fixed error with deploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   basePath: "/med-expert-system-frontend",
   reactStrictMode: true,
-  distDir: './out'
 };
 
 export default nextConfig;
