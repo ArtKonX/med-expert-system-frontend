4804ba9e1c1016c8722ab13fbf8132432b8e201c
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-07-29 20:32:58 +0300

    Fixed base url app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/med-expert-system",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
