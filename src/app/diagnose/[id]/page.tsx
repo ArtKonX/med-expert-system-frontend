c44f4ea5c4c12756694f58d1b2fc61c8f01ee981
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-07-30 00:33:48 +0300

    Trying fixed error with deploy

--- src/app/diagnose/[id]/page.tsx
+++ src/app/diagnose/[id]/page.tsx
@@ -3,41 +3,8 @@ import React from 'react';
 import DiagnosePage from "@/pages-components/DiagnosePage/DiagnosePage";
 import { Metadata } from "next";
 
-import NEXT_BACKEND_URL from '@/environment/environment';
-
 interface DiagnoseParams {
     id: string
-}
-
-interface ResultData {
-    id: string,
-    created_at: Date,
-    description: string,
-    diagnose: string,
-    probability: number,
-    symptoms: string,
-    mainDisease: {
-        disease: {
-            name: string,
-            description: string,
-            category: string,
-            risk_level: number
-        },
-        probability: number,
-    },
-    selectedSymptoms: string,
-    similarDiseases: {
-        id: number,
-        disease: {
-            name: string
-        },
-        probability: number
-    }[]
-}
-
-interface ResultsData {
-    success: boolean,
-    data: ResultData[]
 }
 
 export const metadata: Metadata = {
@@ -45,31 +12,7 @@ export const metadata: Metadata = {
     description: 'Созраненный результат исследования',
 };
 
-export const revalidate = 0;
-
-export async function generateStaticParams(): Promise<{ id: string }[]> {
-
-    try {
-        const response = await fetch(`${NEXT_BACKEND_URL}/results`);
-
-        const results = await response.json() as ResultsData;
-
-        if (results.success && results.data.length) {
-            return results.data.map((result) => ({
-                id: result.id.toString()
-            }));
-        }
-
-        return [{
-            id: 'null'
-        }];
-    } catch (err) {
-        console.error('Ошибка при получения результатов исследований:', err);
-        return [{
-            id: 'null'
-        }];
-    }
-}
+export const dynamicParams = false
 
 const Diagnose = async (
     { params }: { params: Promise<DiagnoseParams> }) => {
